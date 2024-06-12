ed76e34539963e6a6b0e5bf4a2f21ef11c6f4c5c
Pham Duong <phamduong11@gmail.com>
2024-06-12 08:14:41 +0700

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '123456789' }]);
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    ]);
+    const [filtPersons, setFiltPersons] = useState(persons);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
+    const [filtPerson, setFiltPerson] = useState('');
 
     const addNew = (event) => {
         event.preventDefault();
@@ -14,6 +21,7 @@ const App = () => {
         const newList = [...persons];
         newList.push({ name: newName, number: newNumber });
         setPersons(newList);
+        setFiltPersons(newList);
         setNewName('');
         setNewNumber('');
     };
@@ -26,9 +34,19 @@ const App = () => {
         setNewNumber(event.target.value);
     };
 
+    const handleFiltInput = (event) => {
+        setFiltPerson(event.target.value);
+        const filterList = event.target.value ? persons.filter((person) => person.name.includes(event.target.value)) : persons;
+        setFiltPersons(filterList);
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with: <input onChange={handleFiltInput} value={filtPerson} />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addNew}>
                 <div>
                     name: <input onChange={handleNameInput} value={newName} />
@@ -41,9 +59,9 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person) => {
+            {filtPersons.map((person) => {
                 return (
-                    <p key={person.name}>
+                    <p key={person.id}>
                         {person.name}-{person.number}
                     </p>
                 );
