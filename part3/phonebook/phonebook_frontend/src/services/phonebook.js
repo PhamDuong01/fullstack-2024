bb39f7718f3db520a0bfe11e54edd0434480c6dc
Pham Duong <phamduong11@gmail.com>
2024-06-17 13:08:17 +0700

    3.11 Full Stack Phonebook

--- part3/phonebook/phonebook_frontend/src/services/phonebook.js
+++ part3/phonebook/phonebook_frontend/src/services/phonebook.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const apiURL = import.meta.env.VITE_API_URL;
+const baseUrl = `${apiURL}/api/persons`;
 
 const getAll = async () => {
     try {
