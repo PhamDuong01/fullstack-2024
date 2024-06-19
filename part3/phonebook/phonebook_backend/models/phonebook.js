1d68b32b71d43a66657cdac5982ad1dae50da478
Pham Duong <phamduong11@gmail.com>
2024-06-19 15:27:13 +0700

    3.20*: Phonebook database, step 8

--- part3/phonebook/phonebook_backend/models/phonebook.js
+++ part3/phonebook/phonebook_backend/models/phonebook.js
@@ -25,7 +25,14 @@ const phonebookSchema = new mongoose.Schema({
     },
     number: {
         type: String,
+        validate: {
+            validator: function (v) {
+                return /^(?:\d{2}-\d{6,}|\d{3}-\d{5,})$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
         required: true,
+        minLength: 8,
     },
 });
 
