dc3d4196a0127d80c26d5ff412c4df3747839f40
Pham Duong <phamduong11@gmail.com>
2024-06-21 12:20:01 +0700

    Part 4 init

--- part3/phonebook/phonebook_frontend/src/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ onHandleFiltInput, filtPerson }) => {
-    return (
-        <div>
-            filter shown with: <input onChange={onHandleFiltInput} value={filtPerson} />
-        </div>
-    );
-};
-
-export default Filter;
